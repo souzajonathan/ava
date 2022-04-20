08f0de24bab9ebdc18baabbc53ef7bcac373c713
Jonathan Rodrigues <jonathan.and.forces@gmail.com>
2022-04-20 17:38:03 -0300

    first commit

---
+++ src/database/migrations/1650487026238-CreateAreas.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class CreateAreas1650487026238 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<any> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<any> {
+    }
+
+}
