d361fb7d9f20a7598206b4eab798c199586ce3e2
Jonathan Rodrigues <jonathan.and.forces@gmail.com>
2022-04-26 16:38:06 -0300

    2604

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,14 @@ import "reflect-metadata";
 import express from "express";
 import "./database";
 import { routes } from "./routes";
+import cors from "cors";
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
 app.use(routes);
 
-app.listen(3000, () => console.log("Server is running"));
+app.listen(5001, () => console.log("Server is running"));
