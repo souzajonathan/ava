eef56c46792b48148454b2bfeb47d79847247f14
Jonathan Rodrigues <jonathan.and.forces@gmail.com>
2022-04-28 15:33:28 -0300

    2804

--- src/entities/Area.ts
+++ src/entities/Area.ts
@@ -1,4 +1,4 @@
-import { Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity("areas")
 export class Area {
