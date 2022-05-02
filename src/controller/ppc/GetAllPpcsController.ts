8744c2561e7b38842ba87f6cb6c666e1eed419c7
Jonathan Rodrigues <jonathan.and.forces@gmail.com>
2022-05-02 15:56:03 -0300

    0205

--- src/controller/ppc/GetAllPpcsController.ts
+++ src/controller/ppc/GetAllPpcsController.ts
@@ -3,7 +3,6 @@ import { GetAllPpcsService } from "../../services/ppc/GetAllPpcsService";
 
 export class GetAllPpcsController {
     async handle(request: Request, response: Response) {
-        console.log(request.query);
         const service = new GetAllPpcsService();
 
         const ppcs = await service.execute(request.query?.curso_id as string);
