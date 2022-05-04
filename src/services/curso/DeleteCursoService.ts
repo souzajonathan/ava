95ef1f33c7cb2471e6ef307fa2856db1b7b461a9
Jonathan Rodrigues <jonathan.and.forces@gmail.com>
2022-05-04 16:55:43 -0300

    Delete fixed

--- src/services/curso/DeleteCursoService.ts
+++ src/services/curso/DeleteCursoService.ts
@@ -1,12 +1,18 @@
 import { getRepository } from "typeorm";
 import { Curso } from "../../entities/Curso";
+import { Ppc } from "../../entities/Ppc";
 
 export class DeleteCursoService {
     async execute(id: string) {
         const repo = getRepository(Curso);
+        const repoPpc = getRepository(Ppc);
         
         if(!(await repo.findOne(id))){
             return new Error("Curso não existe!");
+        }
+
+        if(await repoPpc.findOne({where: {curso_id : id}})){
+            return new Error("Curso com ppc's cadastrados");
         }
 
         await repo.delete(id);
--- src/services/ppc/DeletePpcService.ts
+++ src/services/ppc/DeletePpcService.ts
@@ -1,12 +1,24 @@
 import { getRepository } from "typeorm";
+import { CompetHabilidades } from "../../entities/CompetHabilidades";
+import { PerfilEgresso } from "../../entities/PerfilEgresso";
 import { Ppc } from "../../entities/Ppc";
 
 export class DeletePpcService {
     async execute(id: string) {
         const repo = getRepository(Ppc);
+        const repoCompet = getRepository(CompetHabilidades);
+        const repoPerf = getRepository(PerfilEgresso);
         
         if(!(await repo.findOne(id))){
             return new Error("Ppc não existe!");
+        }
+
+        if(await repoCompet.findOne({where: {ppc_id : id}})){
+            return new Error("PPC com competências cadastradas");
+        }
+        
+        if(await repoPerf.findOne({where: {ppc_id : id}})){
+            return new Error("PPC com perfis cadastrados");
         }
 
         await repo.delete(id);
