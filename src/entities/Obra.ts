ae59671803130f29f08d1eb2ddbfbbabb3c5a373
souzajonathan <jonathan.and.forces@gmail.com>
2022-06-06 17:50:10 -0300

    obra children and parent implemented

--- src/entities/Obra.ts
+++ src/entities/Obra.ts
@@ -1,4 +1,4 @@
-import { Column, CreateDateColumn, Entity, JoinColumn, ManyToMany, ManyToOne, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import { Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, OneToMany, PrimaryGeneratedColumn } from "typeorm";
 import { Bibliografia } from "./Bibliografia";
 import { ObraAutor } from "./ObraAutor";
 
