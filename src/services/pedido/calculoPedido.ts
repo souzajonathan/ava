a450ce4315a78724b0776080a97ac9f7eb060439
souzajonathan <jonathan.and.forces@gmail.com>
2022-06-09 16:24:40 -0300

    calculating the subjects to the next module

---
+++ src/services/pedido/calculoPedido.ts
@@ -0,0 +1,26 @@
+import { getRepository } from "typeorm";
+import { Ppc } from "../../entities/Ppc";
+
+type PpcRequest = {
+    id: string;
+    dataLeft: string;
+    dataRight: string;
+};
+
+export class CalculoPedido {
+    async execute({ id, dataLeft, dataRight }: PpcRequest): Promise<Ppc | Error> {
+        const repo = getRepository(Ppc);
+
+        const ppc = await repo.findOne(id);
+
+        const inicioData = new Date(ppc.dataInicio);
+        const dataA = new Date(dataLeft);
+
+
+            /* dateTo.getMonth() -
+            dateFrom.getMonth() +
+            12 * (dateTo.getFullYear() - dateFrom.getFullYear()) */
+
+        return ppc;
+    }
+}
